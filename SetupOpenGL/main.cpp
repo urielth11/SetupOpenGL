1bfcc76fbf91fb5d4952742c15579e107ab0bfa9
Jherson Pando <urielth11@gmail.com>
2024-11-03 23:50:45 -0500

    2 articulaciones 100%
    Palma y dedos. 2 articulaciones 100%

--- SetupOpenGL/main.cpp
+++ SetupOpenGL/main.cpp
@@ -1,6 +1,8 @@
 #include <GLFW/glfw3.h>
 #include <cmath>
 #define STB_IMAGE_IMPLEMENTATION
+
+float distancia = 0.40;
 //#include <stb_image.h>
 
 //Función para configurar e inicializar el entorno de OpenGL
@@ -23,87 +25,140 @@ bool inicializarOpenGL() {
     return true;
 }
 
-void dibujarCuadradoEnZ(int x0, int y0, int z0) {
-    float x = (float)x0 / 10;
-    float y = (float)y0 / 10;
-    float z = (float)z0 / 10;
+void dibujarCuadradoEnZ(int x0, int y0, int z0, bool pulgar) {
+    float x = (float)x0 / 100;
+    float y = (float)y0 / 100;
+    float z = (float)z0 / 100;
 
-    glVertex3f(x, y+x, z);
-    glVertex3f(-x, y+x, z);
-    glVertex3f(-x, y-x, z);
-    glVertex3f(x, y-x, z);
+    if (!pulgar) {
+        for (int i = -2; i <= 1; i++) {
+            glVertex3f(x + distancia * i, y + x, z);
+            glVertex3f(-x + distancia * i, y + x, z);
+            glVertex3f(-x + distancia * i, y - x, z);
+            glVertex3f(x + distancia * i, y - x, z);
+        }
+    }
+    else {
+        glVertex3f(x + distancia * 2, y + x - distancia, z);
+        glVertex3f(-x + distancia * 2, y + x - distancia, z);
+        glVertex3f(-x + distancia * 2, y - x - distancia, z);
+        glVertex3f(x + distancia * 2, y - x - distancia, z);
+    }
 }
 
-void dibujarCuadradoEnX(int x0, int y0, int z0) {
-    float x = (float)x0 / 10;
-    float y = (float)y0 / 10;
-    float z = (float)z0 / 10;
+void dibujarCuadradoEnX(int x0, int y0, int z0, bool pulgar) {
+    float x = (float)x0 / 100;
+    float y = (float)y0 / 100;
+    float z = (float)z0 / 100;
 
-    glVertex3f(x, y-x, z-x);
-    glVertex3f(x, y-x, z+x);
-    glVertex3f(x, y+x, z+x);
-    glVertex3f(x, y+x, z-x);
+    if (!pulgar) {
+        for (int i = -2; i <= 1; i++) {
+            glVertex3f(x + distancia * i, y - x, z - x);
+            glVertex3f(x + distancia * i, y - x, z + x);
+            glVertex3f(x + distancia * i, y + x, z + x);
+            glVertex3f(x + distancia * i, y + x, z - x);
+        }
+    }
+    else {
+        glVertex3f(x + distancia * 2, y - x - distancia, z - x);
+        glVertex3f(x + distancia * 2, y - x - distancia, z + x);
+        glVertex3f(x + distancia * 2, y + x - distancia, z + x);
+        glVertex3f(x + distancia * 2, y + x - distancia, z - x);
+    }
 }
 
-void dibujarCuadradoEnY(int x0, int y0, int z0) {
-    float x = (float)x0 / 10;
-    float y = (float)y0 / 10;
-    float z = (float)z0 / 10;
+void dibujarCuadradoEnY(int x0, int y0, int z0, bool pulgar) {
+    float x = (float)x0 / 100;
+    float y = (float)y0 / 100;
+    float z = (float)z0 / 100;
 
-    glVertex3f(-x, y, -z);
-    glVertex3f(x, y, -z);
-    glVertex3f(x, y, z);
-    glVertex3f(-x, y, z);
+    if (!pulgar) {
+        for (int i = -2; i <= 1; i++) {
+            glVertex3f(-x + distancia * i, y, -z);
+            glVertex3f(x + distancia * i, y, -z);
+            glVertex3f(x + distancia * i, y, z);
+            glVertex3f(-x + distancia * i, y, z);
+        }
+    }
+    else {
+        glVertex3f(-x + distancia * 2, y - distancia, -z);
+        glVertex3f(x + distancia * 2, y - distancia, -z);
+        glVertex3f(x + distancia * 2, y - distancia, z);
+        glVertex3f(-x + distancia * 2, y - distancia, z);
+    }
 }
 
 
-void dibujarFalange1(int t) {
+void dibujarFalange1(int t, bool pulgar) {
 
     glBegin(GL_QUADS); // Inicia la definición de un conjunto de polígonos de 4 vértices (cuadrados)
     // Definir cada cara del cubo con su color y vértices
 
     glColor3f(0.5f, 0.0f, 0.0f); //Color rojo 
 
-    dibujarCuadradoEnZ(t, 0, t); //Cara frontal
-    dibujarCuadradoEnZ(t, 0, -t);//Cara trasera
-    dibujarCuadradoEnX(t, 0, 0);//Cara derecha
-    dibujarCuadradoEnX(-t, 0, 0);//Cara izquierda
-    dibujarCuadradoEnY(t, -t, t);//Cara inferior
-    dibujarCuadradoEnY(t, t, t);//Cara superior
+    dibujarCuadradoEnZ(t, 0, t, pulgar); //Cara frontal
+    dibujarCuadradoEnZ(t, 0, -t, pulgar);//Cara trasera
+    dibujarCuadradoEnX(t, 0, 0, pulgar);//Cara derecha
+    dibujarCuadradoEnX(-t, 0, 0, pulgar);//Cara izquierda
+    dibujarCuadradoEnY(t, -t, t, pulgar);//Cara inferior
+    dibujarCuadradoEnY(t, t, t, pulgar);//Cara superior
 
     glEnd(); // Termina la definición del cubo
 }
 
-void dibujarFalange2(int t) {
+void dibujarFalange2(int t, bool pulgar) {
 
     glBegin(GL_QUADS); // Inicia la definición de un conjunto de polígonos de 4 vértices (cuadrados)
     // Definir cada cara del cubo con su color y vértices
 
     glColor3f(0.0f, 1.0f, 0.0f); // Color verde 
 
-    dibujarCuadradoEnZ(t, t*2, t); //Cara frontal
-    dibujarCuadradoEnZ(t, t * 2, -t);//Cara trasera
-    dibujarCuadradoEnX(t, t * 2, 0);//Cara derecha
-    dibujarCuadradoEnX(-t, t * 2, 0);//Cara izquierda
-    dibujarCuadradoEnY(t, t * 2, t);//Cara inferior
-    dibujarCuadradoEnY(t, t * 3, t);//Cara superior
+    dibujarCuadradoEnZ(t, t*2, t, pulgar); //Cara frontal
+    dibujarCuadradoEnZ(t, t * 2, -t, pulgar);//Cara trasera
+    dibujarCuadradoEnX(t, t * 2, 0, pulgar);//Cara derecha
+    dibujarCuadradoEnX(-t, t * 2, 0, pulgar);//Cara izquierda
+    dibujarCuadradoEnY(t, t * 2, t, pulgar);//Cara inferior
+    dibujarCuadradoEnY(t, t * 3, t, pulgar);//Cara superior
 
     glEnd(); // Termina la definición del cubo
 }
 
-void dibujarFalange3(int t) {
+void dibujarFalange3(int t, bool pulgar) {
 
     glBegin(GL_QUADS); // Inicia la definición de un conjunto de polígonos de 4 vértices (cuadrados)
     // Definir cada cara del cubo con su color y vértices
 
     glColor3f(0.0f, 0.0f, 1.0f); // Color azul Cara izquierda // Color verde Cara trasera
     
-    dibujarCuadradoEnZ(t, t * 4, t); //Cara frontal
-    dibujarCuadradoEnZ(t, t * 4, -t);//Cara trasera
-    dibujarCuadradoEnX(t, t * 4, 0);//Cara derecha
-    dibujarCuadradoEnX(-t, t * 4, 0);//Cara izquierda
-    dibujarCuadradoEnY(t, t * 4, t);//Cara inferior
-    dibujarCuadradoEnY(t, t * 5, t);//Cara superior
+    dibujarCuadradoEnZ(t, t * 4, t, pulgar); //Cara frontal
+    dibujarCuadradoEnZ(t, t * 4, -t, pulgar);//Cara trasera
+    dibujarCuadradoEnX(t, t * 4, 0, pulgar);//Cara derecha
+    dibujarCuadradoEnX(-t, t * 4, 0, pulgar);//Cara izquierda
+    dibujarCuadradoEnY(t, t * 4, t, pulgar);//Cara inferior
+    dibujarCuadradoEnY(t, t * 5, t, pulgar);//Cara superior
+
+    glEnd(); // Termina la definición del cubo
+}
+
+void dibujarPalma() {
+    float x = (float)-90 / 100;
+    float y = (float)-100 / 100;
+    float z = (float)10 / 100;
+
+    glBegin(GL_QUADS); // Inicia la definición de un conjunto de polígonos de 4 vértices (cuadrados)
+    // Definir cada cara del cubo con su color y vértices
+
+    glColor3f(0.0f, 1.0f, 1.0f); // Color cian
+
+    /*glVertex3f(x, y + x, z);
+    glVertex3f(-x, y + x, z);
+    glVertex3f(-x, y - x, z);
+    glVertex3f(x, y - x, z);*/
+
+    glVertex3f(0.5f, -0.1f, 0.1f);
+    glVertex3f(-0.9f, -0.1f, 0.1f);
+    glVertex3f(-0.9f, -0.9f, 0.1f);
+    glVertex3f(0.5f, -0.9f, 0.1f);
 
     glEnd(); // Termina la definición del cubo
 }
@@ -115,7 +170,7 @@ int main() {
     }
 
     // Crear la ventana GLFW
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Cubo 3D Estático", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Mano Humana", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -154,19 +209,34 @@ int main() {
         glTranslatef(0.0f, 0.0f, -3.0f); //Alejar la cámara
 
         glPushMatrix();// Guardar la matriz actual
-        glRotatef(rotacion1, 1.0f, 0.0f, 0.0f); //rotar el cubo para una mejor visualizaciòn
-        dibujarFalange1(2);//Dibujamos el cubo
+        glRotatef(rotacion1, 1.0f, 0.1f, 0.0f); //rotar el cubo para una mejor visualizaciòn
+        dibujarFalange1(10, false);//Dibujamos el cubo
         glPopMatrix();// Restaurar la matriz anterior para que otros objetos no se roten
         
         glPushMatrix();// Guardar la matriz actual
-        glRotatef(rotacion2, 1.0f, 0.0f, 0.0f); //rotar el cubo para una mejor visualizaciòn
-        dibujarFalange2(2);//Dibujamos el cubo
+        glRotatef(rotacion2, 1.0f, 0.1f, 0.0f); //rotar el cubo para una mejor visualizaciòn
+        dibujarFalange2(10, false);//Dibujamos el cubo
         glPopMatrix();// Restaurar la matriz anterior para que otros objetos no se roten
 
         glPushMatrix();// Guardar la matriz actual
-        glRotatef(rotacion3, 1.0f, 0.0f, 0.0f); //rotar el cubo para una mejor visualizaciòn
-        dibujarFalange3(2);//Dibujamos el cubo
+        glRotatef(rotacion3, 1.0f, 0.1f, 0.0f); //rotar el cubo para una mejor visualizaciòn
+        dibujarFalange3(10, false);//Dibujamos el cubo
         glPopMatrix();// Restaurar la matriz anterior para que otros objetos no se roten
+
+
+
+        glPushMatrix();// Guardar la matriz actual
+        glRotatef(rotacion3, 1.0f, -1.0f, 0.0f); //rotar el cubo para una mejor visualizaciòn
+        dibujarFalange1(10, true);//Dibujamos el cubo
+        glPopMatrix();// Restaurar la matriz anterior para que otros objetos no se roten
+
+        glPushMatrix();// Guardar la matriz actual
+        glRotatef(rotacion3, 1.0f, -1.0f, 0.0f);//rotar el cubo para una mejor visualizaciòn
+        dibujarFalange2(10, true);//Dibujamos el cubo
+        glPopMatrix();// Restaurar la matriz anterior para que otros objetos no se roten
+
+        dibujarPalma();
+
 
         // Intercambiar buffers y actualizar eventos
         // Comprobar si la tecla espacio está presionada
@@ -174,11 +244,11 @@ int main() {
             rotacion1 += 0.005f;
             rotacion2 += 0.006f;
             rotacion3 += 0.007f;
-            //anguloRotacion += 90.0f;  // Aumentar el ángulo de rotación
+            
             if (rotacion1 >= 100.0f) {  // Resetear si supera 360 grados
-                rotacion1 = 0;
-                rotacion2 = 0;
-                rotacion3 = 0;
+                rotacion1 = 100.0f;
+                rotacion2 = 100.0f + (100.0f / 5);
+                rotacion3 = 100.0f + (2 * (100.0f / 5));
             }
         }
 
@@ -186,7 +256,6 @@ int main() {
             rotacion1 -= 0.005f;
             rotacion2 -= 0.006f;
             rotacion3 -= 0.007f;
-            //anguloRotacion += 90.0f;  // Aumentar el ángulo de rotación
             if (rotacion1 <= 0.0f) {  // Resetear si supera 360 grados
                 rotacion1 = 0;
                 rotacion2 = 0;
